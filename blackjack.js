4a18195c710f696d5a8d4f2c8915e3539ebb6700
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-02-16 21:03:10 -0600

    Betting and one hand are working

--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,15 @@
+//Need to grab DOM elements to append to
+const dealButton = document.getElementById("deal-button");
+const hitButton = document.getElementById("hit-button");
+const stayButton = document.getElementById("stay-button");
+const betButtons = document.querySelectorAll(".bet");
+const playerHandDiv = document.getElementById("player-hand");
+const dealerHandDiv = document.getElementById("dealer-hand");
+const playerStatusElement = document.getElementById("player-status");
+const dealerStatusElement = document.getElementById("dealer-status");
+const instructions = document.getElementById("instructions");
+const bankRollElement = document.getElementById("bankroll");
+
 class BlackjackGame {
     constructor(bankRoll) {
         this.bankRoll = bankRoll;
@@ -5,8 +17,11 @@ class BlackjackGame {
         this.dealerHand = [];
         this.deck = new Deck();
         this.deck.shuffleDeck();
+        this.betAmount = 0;
+        hitButton.style.visibility = "hidden";
+        stayButton.style.visibility = "hidden";
+        dealButton.disabled = true;
     }
-    //Need to grab DOM elements to append to
 
     getHandValue(playerHand) {
         let currentHand = [...playerHand];
@@ -18,7 +33,7 @@ class BlackjackGame {
                 currentHand.push(removedCard[0]);
             }
         });
-        console.log(currentHand);
+        //console.log(currentHand);
 
         currentHand.forEach((card) => {
             if (card.rank === "K" || card.rank === "Q" || card.rank === "J") {
@@ -37,19 +52,20 @@ class BlackjackGame {
         return handValue;
     }
 
-    displayHandValue(playerHand, domElement) {
-        //console.log(playerHand, domElement);
-        domElement.textContent = " Value : " + this.getHandValue(playerHand);
-    }
-
-    displayHand(cards, domElement, isDealersFirstHand) {
+    displayHand(cards, playerType, isDealersFirstHand) {
         let currentCards = [...cards];
         //console.log(currentCards);
+        let domElement;
+        if (playerType === "player") {
+            domElement = playerHandDiv;
+        } else {
+            domElement = dealerHandDiv;
+        }
         domElement.innerHTML = "";
 
         //hide one of the dealers cards
         if (isDealersFirstHand) {
-            currentCards.pop();
+            currentCards.shift();
             let cardElement = document.createElement("div");
             cardElement.className = "card card-back";
             cardElement.innerHTML = `
@@ -61,7 +77,7 @@ class BlackjackGame {
         currentCards.forEach((card) => {
             let cardElement = document.createElement("div");
             if (card.suit === "Diamond" || card.suit === "Heart") {
-                cardElement.className = "card red"; //${card.suit.toLowerCase()}
+                cardElement.className = "card red";
             } else {
                 cardElement.className = "card";
             }
@@ -73,50 +89,154 @@ class BlackjackGame {
              <div class='card-footer'>${card.rank}</div>`;
             domElement.appendChild(cardElement);
         });
+
+        if (playerType === "player") {
+            this.displayHandValue(currentCards, playerStatusElement);
+        } else {
+            this.displayHandValue(currentCards, dealerStatusElement);
+        }
     }
 
-    playGame() {
-        const playerStatusElement = document.getElementById("player-status");
+    displayHandValue(playerHand, domElement) {
+        domElement.textContent = " Value: " + this.getHandValue(playerHand);
+    }
+
+    //and bets
+    updateStatus(statusMessage, gameEnding, isBlackjack) {
+        if (isBlackjack) {
+            this.betAmount *= 1.5;
+        }
+        //player wins, dealer wins, push
+        switch (gameEnding) {
+            case "player wins":
+                //can add localStorage here
+                this.bankRoll += this.betAmount;
+                break;
+            case "dealer wins":
+                this.bankRoll -= this.betAmount;
+                break;
+        }
+        instructions.innerHTML = statusMessage;
+        bankRollElement.textContent = `$${this.bankRoll}`;
+        // new BlackjackGame(this.bankRoll);
+        console.log("HERE");
+        // this.newDeal();
+    }
+
+    newDeal() {
+        // intialize the Bankroll
+        // and the bet
+        // hide the deal and beat buttons
+        // make visible the hit and stay (and split)
+
+        // this.playerHand = [];
+        // this.dealerHand = [];
+        // this.deck = new Deck();
+        // this.deck.shuffleDeck();
+        // this.betAmount = 0;
+        // hitButton.style.visibility = "hidden";
+        // stayButton.style.visibility = "hidden";
+        // dealButton.style.visibility = "visible";
+        // dealButton.disabled = true;
+
+        betButtons.forEach((button) => {
+            button.addEventListener("click", (e) => {
+                this.betAmount += parseInt(button.value);
+                dealButton.disabled = false;
+                console.log(this.betAmount);
+            });
+        });
+        dealButton.addEventListener("click", (e) => {
+            dealButton.style.visibility = "hidden";
+            hitButton.style.visibility = "visible";
+            stayButton.style.visibility = "visible";
+            this.playerTurn();
+        });
+    }
+
+    playerTurn() {
         //first move
         this.playerHand.push(this.deck.cards.pop());
         this.dealerHand.push(this.deck.cards.pop());
         this.playerHand.push(this.deck.cards.pop());
         this.dealerHand.push(this.deck.cards.pop());
         // console.log(this.playerHand);
-        console.log("Dealer hand: " + this.dealerHand);
+        console.log(this.dealerHand);
 
         //Display hands
-        const playerHandDiv = document.getElementById("player-hand");
-        this.displayHand(this.playerHand, playerHandDiv, false);
-        this.displayHandValue(this.playerHand, playerStatusElement);
-        const dealerHandDiv = document.getElementById("dealer-hand");
-        this.displayHand(this.dealerHand, dealerHandDiv, true);
-
+        this.displayHand(this.playerHand, "player", false);
+        //this.displayHandValue(this.playerHand, playerStatusElement);
+        this.displayHand(this.dealerHand, "dealer", true);
+        this.checkForBlackjack();
         //wire up the control buttons
-        const hitButton = document.getElementById("hit-button");
-        const stayButton = document.getElementById("stay-button");
-        const betButtons = document.querySelectorAll(".bet");
-
         hitButton.addEventListener("click", (e) => {
             this.playerHand.push(this.deck.cards.pop());
-            this.displayHand(this.playerHand, playerHandDiv, false);
+            this.displayHand(this.playerHand, "player", false);
             this.displayHandValue(this.playerHand, playerStatusElement);
+            this.checkForPlayerWin();
         });
         stayButton.addEventListener("click", (e) => {
-            alert("Stay button clicked");
             //Dealer turn
+            this.dealerTurn();
         });
-        betButtons.forEach((button) => {
-            button.addEventListener("click", (e) => {
-                console.log(button.value);
-            });
-        });
+    }
 
-        // console.log("Player Value: " + this.getHandValue(this.playerHand));
-        // console.log("Dealer Value: " + this.getHandValue(this.dealerHand));
-        //then dealer turn
+    dealerTurn() {
+        this.displayHand(this.dealerHand, "dealer", false);
+
+        while (this.getHandValue(this.dealerHand) < 17) {
+            this.dealerHand.push(this.deck.cards.pop());
+            this.displayHand(this.dealerHand, "dealer", false);
+        }
+        let playerHandValue = this.getHandValue(this.playerHand);
+        let dealerHandValue = this.getHandValue(this.dealerHand);
+
+        if (dealerHandValue > 21) {
+            this.updateStatus("Dealer Busts. You Win.", "player wins", false);
+        } else if (playerHandValue === dealerHandValue) {
+            this.updateStatus("It's a tie. You Push.", "push", false);
+        } else if (playerHandValue > dealerHandValue) {
+            this.updateStatus("You Win.", "player wins", false);
+        } else {
+            this.updateStatus("Dealer Wins.", "dealer wins", false);
+        }
+    }
+
+    checkForBlackjack() {
+        let playerHandValue = this.getHandValue(this.playerHand);
+        let dealerHandValue = this.getHandValue(this.dealerHand);
+
+        if (playerHandValue === 21 && dealerHandValue === 21) {
+            this.updateStatus("Both have Blackjack Push", "push", false);
+            this.displayHand(this.dealerHand, "dealer", false);
+        } else if (playerHandValue === 21) {
+            this.updateStatus(
+                "You have Blackjack! You win.",
+                "player wins",
+                true
+            );
+            this.displayHand(this.dealerHand, "dealer", false);
+            //this.newDeal();
+        } else if (dealerHandValue === 21) {
+            this.updateStatus(
+                "Dealer has Blackjack! You lose.",
+                "dealer wins",
+                false
+            );
+            this.displayHand(this.dealerHand, "dealer", false);
+        }
+    }
+    checkForPlayerWin() {
+        let playerHandValue = this.getHandValue(this.playerHand);
+
+        if (playerHandValue === 21) {
+            this.dealerTurn();
+        } else if (playerHandValue > 21) {
+            this.updateStatus("You busted! You lose.", "dealer wins", false);
+            this.displayHand(this.dealerHand, "dealer", false);
+        }
     }
 }
 
 let game = new BlackjackGame(1000);
-game.playGame();
+game.newDeal();
